91008c76e9a014ba7af9d2eb0d0cb2453c872dfc
Deborah Lyra <deborahp.lyra@gmail.com>
2024-02-01 16:00:07 -0300

    Startting tic-tac-toe

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import GamePage from './components/gamePage/GamePage'
+
+function App() {
+
+  return (
+    <>
+     <GamePage></GamePage>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/gamePage/GamePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function GamePage() {
+  return (
+    <div class= "container">
+        <h1>Tic-Tac-Toe</h1>
+        <span>
+        <p>Created by Deborah Lyra </p>
+       <a href='https://github.com/DeborahLyra'> <i class="fab fa-github"></i></a>
+        </span>
+    </div>
+  )
+}
+
+export default GamePage
