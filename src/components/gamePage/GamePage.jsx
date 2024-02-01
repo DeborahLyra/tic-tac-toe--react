63e30b3cd33f9a506d0946c2a0b75c4562ab8f11
Deborah Lyra <deborahp.lyra@gmail.com>
2024-02-01 16:28:50 -0300

    starting game page

--- src/components/gamePage/GamePage.jsx
+++ src/components/gamePage/GamePage.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
+import styled from 'styled-components';
 
 function GamePage() {
   return (
-    <div class= "container">
-        <h1>Tic-Tac-Toe</h1>
-        <span>
+    <Div>
+        <H1>Tic-Tac-Toe</H1>
+        <Span>
         <p>Created by Deborah Lyra </p>
-       <a href='https://github.com/DeborahLyra'> <i class="fab fa-github"></i></a>
-        </span>
-    </div>
+       <a href='https://github.com/DeborahLyra' target='_blank'> <i class="fab fa-github"></i></a>
+        </Span>
+    </Div>
   )
 }
+
+export const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 3rem;
+  gap: 15px;
+`;
+
+export const H1 = styled.h1`
+ font-size: 2.2rem
+`;
+
+export const Span = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+  font-size: 1.5rem;
+`;
+
+
 
 export default GamePage
