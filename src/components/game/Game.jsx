f2f8f19edd990a327aba0ada058dc9005058c475
Deborah Lyra <deborahp.lyra@gmail.com>
2024-02-09 17:59:42 -0300

    set winner

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,23 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GameOpition from "../gameOpition/GameOpition"
 import styled from 'styled-components';
 import Icon from "../icon/Icon";
 
-
+//possibilidades de vitórias
+const winerTable = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+]
 
 function Game() {
 
   const [gameState, setGameState] = useState(Array(9).fill(0));
-  const [currentPlayer, setCurrentPlayer] = useState(1)
+  const [currentPlayer, setCurrentPlayer] = useState(1) //1 = circulo | -1 = x 
+  const [winner, setWinner] = useState(0) // 0 = sem campeão | 1 = circulo ganhou | -1 = x ganhou
 
   const handleClick = (index) => {
-    if (gameState[index] === 0) {
+    if (gameState[index] === 0 && winner === 0) {
       let newGameState = [...gameState];
       newGameState[index] = currentPlayer;
-      setCurrentPlayer(currentPlayer === 1 ? -1 : 1);
       setGameState(newGameState)
     }
   }
+
+  const verifyGame = () => {
+    winerTable.forEach((line) => {
+      const values = line.map((value) => gameState[value]); //o que está na tabela são as posicoes do array, então ele acaba pegando o item na posicao tal 
+      const acc = values.reduce((acc, value)=> acc + value) // aqui soma, pq se der 3 ou -3 tem um vencedor
+      if(acc === 3 || acc === -3) {setWinner(acc/3)}
+    })
+  }
+
+  useEffect(() => {
+    setCurrentPlayer(currentPlayer === 1 ? -1 : 1);
+    verifyGame()
+  }, [gameState])
 
   return (
     <DivContainer>
